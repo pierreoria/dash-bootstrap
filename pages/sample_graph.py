08095670c0428416d57b445b25e88d5aefdb2323
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2024-12-17 11:05:23 -0300

    Update sample_graph.py

--- pages/sample_graph.py
+++ pages/sample_graph.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 from defaultlayouts import globalconstants
+from plotly_v0 import my_plot
 
 register_page(module=__name__,
               name="Sample Graph",
@@ -12,10 +13,76 @@ register_page(module=__name__,
 
 load_figure_template(["bootstrap", "bootstrap_dark"])
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-df["lifeExp"] = df["lifeExp"].round(1)
-df["gdpPercap"] = df["gdpPercap"].astype(int)
+# ----------------------------------------------------------------------------------------------
+
+# Load historical data
+def load_data(filename:str):
+    with open(f"{filename}.json") as f:
+        data = json.load(f)  # Parse the entire JSON array
+    return pd.DataFrame(data)
+
+def get_last_entry(filename):
+    with open(f"{filename}.json", "r") as file:
+        data = json.load(file)
+    sorted_keys = sorted(data.keys(), key=int)
+    last_key = sorted_keys[-1]
+    last_entry = data[last_key]
+    return pd.DataFrame([last_entry])
+
+def create_gauge(value, title, big: bool):
+    fig = go.Figure(go.Indicator(
+        mode="gauge+number+delta",
+        value=value * 100,
+        gauge={
+            'axis': {'range': [0, 100], 'tickwidth': 2, 'tickcolor': "darkblue"},
+            'bar': {'color': "darkblue"},
+            'bgcolor': "white",
+            'borderwidth': 2,
+            'bordercolor': "gray",
+            'steps': [
+                {'range': [0, 50], 'color': "lightgray"},
+                {'range': [50, 75], 'color': "gold"},
+                {'range': [75, 100], 'color': "crimson"}
+            ],
+        },
+        delta={'reference': value, 'relative': False},
+        title={'text': title, 'font': {'size': 20}}
+    ))
+
+    if not big:
+        fig.update_layout(
+            width=400, height=400,
+            margin=dict(t=50, b=50, l=50, r=50),
+            paper_bgcolor="white",
+            font={'color': "darkblue", 'family': "Arial"}
+        )
+    else:
+        fig.update_layout(
+            width=600, height=500,
+            margin=dict(t=50, b=50, l=50, r=50),
+            paper_bgcolor="white",
+            font={'color': "darkblue", 'family': "Arial"}
+        )
+
+    return fig
+
+# Load data once
+legislative_df = get_last_entry("Legislative")
+media_df = get_last_entry("Media")
+latest_legislative_data = legislative_df.iloc[-1]
+latest_media_data = media_df.iloc[-1]
+
+risk_gauge = create_gauge(latest_legislative_data["score"]*0.6 + latest_media_data["score"]*0.4, "Regulatory Risk Index",True)
+legislative_gauge = create_gauge(latest_legislative_data["score"], "Legislative Activity Score", False)
+media_gauge = create_gauge(latest_media_data["score"], "Media Activity Score", False)
+legislative_plot = my_plot("Legislative")
+media_plot = my_plot("Media")
+
+
+# ----------------------------------------------------------------------------------------------
+
+
+
 
 # App layout
 layout = [
@@ -29,24 +96,10 @@ layout = [
                 " shows how controls can be duplicated on a custom pop-up sidebar"
             ])
         ]),
-        dbc.Row([
-            dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
-                        value='lifeExp',
-                        inline=True,
-                        id='radio-buttons-final', className='bg-info rounded-1 my-2')
-        ],),
         dbc.Row(
             [
                 dbc.Col([
-                    dash_table.DataTable(
-                        data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'},
-                    )
-                    ],
-                    md=6,
-                    id='col-datatable',
-                ),
-                dbc.Col([
-                    dcc.Graph(figure={}, id='my-first-graph-final')
+                    dcc.Graph(figure=risk_gauge, id='my-first-graph-final')
                     ],
                     id='col-graph',
                     md=6,
@@ -55,38 +108,4 @@ layout = [
             className='dbc',
         ), 
     ], className="page-body"),
-    # -- The custom pop-up drawer. Duplicates the radio buttons
-    dbc.Offcanvas(
-        id={"type":"drawer", "page": __name__}, # Needed for the open/close callbacks
-        title="Graph Controls",
-        children=
-        [
-            dcc.Link("Home", href="/"),
-            dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
-                        value='lifeExp',
-                        id='radio-buttons-popup',
-                        className="bg-info rounded-1 my-3"
-            )
-        ],
-        style={   # Styling .page-navbar in CSS doesn't seem to work to do this...
-            "top":f"{globalconstants.header_height}px",
-            "bottom":f"{globalconstants.footer_height}px",
-        }
-    ),
 ]
-# -------------------------------------------------------
-# Handle radio buttons. Keep the two duplicates in sync
-@callback(
-    Output('my-first-graph-final', 'figure'),
-    Output('radio-buttons-final', 'value'),
-    Output('radio-buttons-popup', 'value'),
-    Input('radio-buttons-final', 'value'),
-    Input('radio-buttons-popup', 'value'),
-    Input("core-lightswitch", "n_clicks"),
-)
-def update_graph(col_body, col_popup, nclicks):
-    trigger = ctx.triggered_id
-    col_chosen = col_body if trigger == 'radio-buttons-final' else col_popup
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg', 
-                       template="bootstrap_dark" if nclicks % 2 else "bootstrap")
-    return fig, col_chosen, col_chosen
